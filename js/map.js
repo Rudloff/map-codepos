f58d4340984a1a3ad1b55124c8c8702bc07a3b64
Pierre Rudloff <contact@rudloff.pro>
2014-12-16 18:12:02 +0100

    Layer path

--- js/map.js
+++ js/map.js
@@ -10,10 +10,10 @@ function init() {
             [49.3180, 8.421]
         ]
     }).setView([48.5747, 7.7448], 11);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    L.tileLayer('http://localhost/map-codepos/php-mbtiles/codes/{z}/{x}/{y}.png', {
+    L.tileLayer('php-mbtiles/codes/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.data.gouv.fr/fr/datasets/fond-de-carte-des-codes-postaux/">Emc3</a>'
     }).addTo(map);
 }
